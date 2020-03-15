2b4aa9960c41438dbfbb3ea6bbf28891a524d67d
zygouras <zygouras@ceid.upatras.gr>
2020-03-15 17:27:38 +0200

    remove old key

--- src/components/routesMap/routesMap.js
+++ src/components/routesMap/routesMap.js
@@ -67,7 +67,7 @@ const RoutesMap = (props) => {
 
 export default compose(
     withProps({
-        googleMapURL:"https://maps.googleapis.com/maps/api/js?key=AIzaSyC2HVuMFUP1m-0YxdgCHZE70CYAzVFXBIs&v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL:"https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `500px` }} />,
